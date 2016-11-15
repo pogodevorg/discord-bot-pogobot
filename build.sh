ea67d6be15299484dfcaaaee6603cf7716ab6834
fkndean <admin@darkmatter.zone>
2016-11-14 19:17:30 -0500

    Attempt 3

--- build.sh
+++ build.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
-
-#exit if any command fails
-set -e
-artifactsFolder="./artifacts"
-
-if [ -d $artifactsFolder ]; then  
-  rm -R $artifactsFolder
-fi
-
+git submodule update --init --recursive
 dotnet restore
+cd src/NadekoBot/
 
-dotnet build ./src/NadekoBot -c Release
-
-revision=${TRAVIS_JOB_ID:=1}  
-revision=$(printf "%04d" $revision) 
-
-dotnet pack ./src/NadekoBot -c Release -o ./artifacts --version-suffix=$revision  
+dotnet build
